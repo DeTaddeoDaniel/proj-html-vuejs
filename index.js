4b578de640d7644c30a42c574b2c18ca408c98ff
Daniel2711 <detaddeo.d@gmail.com>
2021-02-15 18:42:15 +0100

    Sezione 2: stampa elementi dinamicamente con vue

---
+++ index.js
@@ -0,0 +1,23 @@
+new Vue({
+    el: '#app',
+
+    data:{
+        numeri:[
+            {
+                numero:2032,
+                testo: "Volontari nel mondo",
+                icon:`<i class="far fa-heart"></i>`
+            },
+            {
+                numero:132,
+                testo: "Active project",
+                icon:`<i class="fas fa-globe">`
+            },
+            {
+                numero:'3.2 M',
+                testo: "Donated",
+                icon:`<i class="fas fa-dollar-sign"></i>`
+            },
+        ]
+    }
+})
